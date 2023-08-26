fd5519fd6f3f5277cecf9135de7b3dec58c417ac
Elena Ziani <zianielena+github@gmail.com>
2023-08-26 15:42:52 +0200

    index navbar

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/style.css">
+    <title>Movies List</title>
+</head>
+<body>
+
+    <nav class="navbar">
+        <div class="logo"> 
+            <a href="#"><img src="/assets/img/Movie-List-black.png" alt=""></a>
+        </div>
+        <div class="nav-btn">
+            <button><a href="#">Se connecter</a></button>
+        </div>
+    </nav>
+    
+</body>
+</html>
