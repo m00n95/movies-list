2e8ebf102384a990ab6c238aff13d8492ace4b58
Elena Ziani <zianielena+github@gmail.com>
2023-09-10 19:38:52 +0200

    oups

--- homepage.php
+++ homepage.php
@@ -26,7 +26,7 @@ $movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <h2>Présentation</h2>
 
 <div style="margin-left: 50px; margin-right: 50px;">
-  <p>Bienvenue sur Movie List, le site pour gardez une trace des films que vous avez vu. <br>
+  <p>Bienvenue sur Movie List, le site pour garder une trace des films que vous avez vu. <br>
    Sur ce site vous pourrez ajouter vos propres films et les modifier si besoin. De plus, pour chacun de vos films vous aurez l'occasion d'ajouter un commentaire accompagné d'une note de votre choix.</p>
   <p>Commencez dès maintenant votre liste de films !</p>
   <a href="/CRUD/create_movie.php"><button class="btn btn-secondary">Ajouter un film !</button></a>
